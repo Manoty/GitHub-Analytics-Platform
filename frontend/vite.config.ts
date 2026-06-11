d5d1ff6e69eff4a18c4e314d91cba7ad2c2fbb25
Kevin Manoti <kevinnoty21@gmail.com>
2026-06-11 14:16:30 +0300

    Vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: { "@": path.resolve(__dirname, "./src") },
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    proxy: {
+      "/api": {
+        target: "http://backend:8000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
